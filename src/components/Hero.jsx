9c1eaa6fc2215ea51fc40b10cb3b4d350bc2c5b4
Darion Thompson <darion.thompson@ufl.edu>
2023-01-18 13:43:35 -0500

    removed gator graphic and added contact me button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,10 @@
 import { Box, Button, styled, Typography } from "@mui/material";
-
 import { Link } from "react-scroll";
-import reactImg from "../media/florida-gators-logo-png-transparent.png";
-
-
 import React from "react";
-import { borderRadius } from "@mui/system";
+import About from "../components/About.jsx";
+
 
 const Hero = () => {
-const CustomButton = styled(Button)(({theme}) => ({
-  border: "3px solid transparent",
-  backgroundColor: "#00C7FF",
-  color: "white",
-  width: "20%",
-  borderRadius: "25px",
-  "&:hover":{
-    border: "3px solid white",
-    backgroundColor: "transparent",
-  },
-  [theme.breakpoints.down("md")]:{
-    width: "35%",
-  },
-  [theme.breakpoints.down("sm")]:{
-    width: "50%",
-  },
-
-}));
 
 const CustomTitle = styled(Typography)(({theme}) => ({
   color: "white",
@@ -43,10 +22,10 @@ const CustomTitle = styled(Typography)(({theme}) => ({
     id="home"
     >
       <CustomTitle sx={{color: "white", textAlign: "Center", mb:2 }} variant="h4">
-        I'm Darion Thompson
+        Darion Thompson
       </CustomTitle>
       <Typography sx={{color: "white", textAlign: "Center", mb:2 }} variant="h6">
-        <span style={{color: "#ff895d"}}>Biology</span> and <span style={{color: "#ff895d"}}>Computer Science</span> student
+        <span style={{color: "#5fc9f3"}}>Biology</span> and <span style={{color: "#5fc9f3"}}>Computer Science</span>
       </Typography>
     <Box
      sx={{
@@ -57,12 +36,18 @@ const CustomTitle = styled(Typography)(({theme}) => ({
       gap: "1rem",
      }}
      >
-      <Box
-      component="img"
-      sx={{height:150, width:200, mt:4, mb:4}}
-      src={reactImg}
-      />
-      <CustomButton>Contact Me</CustomButton>
+      <Box sx={{height:100, width:500, mt:4, mb:4}}>
+      <About/>
+
+      </Box>
+      
+      
+     <Link to="ContactForm" className="link" smooth>
+      <Button variant="contained">
+      Contact Me
+      </Button>
+    </Link>
+   
     </Box>
   </Box>
   );
