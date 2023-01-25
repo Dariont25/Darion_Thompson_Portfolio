bf3217197c92aa4d5a0d01aebd2d0c782db93d32
Darion Thompson <darion.thompson@ufl.edu>
2023-01-25 11:24:56 -0500

    updated about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import React from "react";
 const About = () => {
   return <Box>
 <Typography className="about" sx={{color: "white", textAlign:"center"}}> 
-  I am a recent graduate from the University of Florida. I have an interest in biotechnology and full stack development!
+  I am a recent graduate from the University of Florida. I have an interest in biotechnology and software development!
   </Typography>
   </Box>;
 };
